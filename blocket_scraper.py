7c856179f57924042e312ad0e95fa4e07e652f34
eskilWinstedt <eggewinstedt@gmail.com>
2019-10-08 12:05:46 +0200

    Add a list of removed ads and newly removed ads

--- blocket_scraper.py
+++ blocket_scraper.py
@@ -3,6 +3,22 @@ from bs4 import BeautifulSoup
 import time
 import os
 
+# To do:
+# *See when ads are removed
+# *Download ads information. 
+# 
+#
+
+
+class Ad:
+    def __init__(self):
+        self.upload_time
+        self.relative_url
+        self.location
+        self.price
+        self.titel
+        self.description
+
 
 class Monitored_category:
     def __init__(self, url, headers = {}):
@@ -11,6 +27,7 @@ class Monitored_category:
         self.headers = headers
         
         self.ad_links = []
+        self.removed_ad_links = []
         self.fetch_all()
         self.update_ad_links()
     
@@ -21,24 +38,30 @@ class Monitored_category:
         return BeautifulSoup(html)
     
     def fetch_all(self):
-        '''Fetches all pages in one category and saves them as soups in a list where every element is on page'''
+        '''Fetches all pages in one category and saves them as soups in a list where every element is one page'''
         self.page_soups = []
         self.page_soups.append(self.fetch(self.url))
+        page_links = [self.url]
 
         page_nav_div = self.page_soups[0].find('div', attrs={'class': 'Pagination__Buttons-uamu6s-3'})      # Get the div with the page-nav buttons
         number_pages = len(page_nav_div.find_all('a'))
-        for page_index in range(2, number_pages + 2):       # Starting at index 2 because page 1 is already fetched
-            self.page_soups.append(self.fetch(self.url + '&page=' + str(page_index)))
+        for page_index in range(2, number_pages + 1):       # Starting at index 2 because page 1 is already fetched
+            page_link = self.url + '&page=' + str(page_index)
+            self.page_soups.append(self.fetch(page_link))
 
     def update_ad_links(self): 
         '''Finds new ads and saves their links'''
         self.new_ad_links = []
-
+        self.newly_removed_ad_links = self.ad_links.copy()        # Don't pass the reference
         for page_soup in self.page_soups:
             for ad in page_soup.findAll('div', attrs={'class': 'styled__Wrapper-sc-1kpvi4z-0 eDiSuB'}):     # Loop over every ad container
-                if not ad['to'] in self.ad_links:            # Attribute 'to' is the relative link to the ad
-                    self.ad_links.append(ad['to'])               
+                if not (ad['to'] in self.ad_links):       # Attribute 'to' is the relative link to the ad
+                    self.ad_links.append(ad['to'])
                     self.new_ad_links.append(ad['to'])
+                else:
+                    self.newly_removed_ad_links.remove(ad['to'])
+        self.removed_ad_links += self.newly_removed_ad_links
+
 
 
 headers = {}
@@ -46,13 +69,16 @@ headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20
 bugs = Monitored_category('https://beta.blocket.se/annonser/hela_sverige/fordon/bilar?cb=40&cg=1020&st=s&cbl1=17')
 
 
-update_delay = 10   # Seconds
+update_delay = 7 * 60   # Seconds
 
 while True:
     bugs.fetch_all()
     bugs.update_ad_links()
     for new_ad_link in bugs.new_ad_links:
         os.system('start chrome beta.blocket.se' + new_ad_link)
-    print('Last updated ' + time.strftime('%H:%M:%S'))
-    bugs.fetch_all()
+        break
+    print('\nLast updated ' + time.strftime('%H:%M:%S'))
+    print('Removed ad links: ' + str(bugs.removed_ad_links))
+    print('Number of ads: ' + str(len(bugs.ad_links)))
+    print("Number pages = " + str(len(bugs.page_soups)))
     time.sleep(update_delay)
