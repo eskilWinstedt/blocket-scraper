2f2e0157e30f4c9fddac9ed029f8edd737bf83d7
eskilWinstedt <eggewinstedt@gmail.com>
2020-06-26 23:18:49 +0200

    Fix fetch_all bug. Test refresh_all and fix. Clear up debugging messages.

--- blocket_scraper.py
+++ blocket_scraper.py
@@ -7,10 +7,7 @@ import mysql.connector
 
 '''
 TO DO:
-  bug: Only the last page is fetched atm.
-  Test the refresh_ads method.
-  Add an option to fetch within the refresh_ads method.
-  Clear up some unimportant debugging messages
+    Ad fetching
 '''
 
 
@@ -18,18 +15,19 @@ TO DO:
 DEBUG_MODE = True
 
 
+# --- ADDITIONAL IMPORTS ------------------
 if DEBUG_MODE:
     from inspect import currentframe, getframeinfo
 
 
-# ------ DATABASE CONFIGURATION CONSTANTS --------------------
-
-
+# --- DATABASE CONFIGURATION CONSTANTS --------------------
 DB_NAME = "blocket_scraper"
 DB_HOST = "localhost"
 DB_USER = "root"
 DB_PASSWD = "ANC:s viktiga partikongress"
 
+
+# --- FUNCTIONS ------------------
 def debug(msg, display_line_nr = False):
     global DEBUG_MODE
     if not DEBUG_MODE: return
@@ -50,13 +48,14 @@ def get_ad_id(ad_link):
     return int(ad_link.split('/')[-1].split('.')[0])
 
 
+# --- CLASSES -------------
 class MonitoredCategory:
     def __init__(self, url):
         # All properties are defined here to make them easier to locate
         self.url = url
         self.headers = {}
         self.categories = []
-        self.table = ""     # The name of the SQL table
+        self.db_table = ""     # The name of the SQL table
         self.ad_ids = []
         self.active_ad_links = []
 
@@ -64,7 +63,7 @@ class MonitoredCategory:
         self._get_categories()
         self._get_table_name()
         self._load_active_ads()
-        self.refresh_ads(False)
+        self.refresh_ads(False) # false = don't fetch again
 
     def _fetch(self, url):
         '''Takes an url to a page as the argument, fetches the page and returns it as a soup'''
@@ -81,15 +80,15 @@ class MonitoredCategory:
     def _fetch_all(self):
         '''Fetches all pages in one category and saves them as soups in a list where every element is one page'''
         self.page_soups = [self._fetch(self.url + '&page=100')]
-        page_nav_div = self.page_soups[0].find("div", attrs={"class": "Pagination__Buttons-uamu6s-3"})      # Get the div with the page-nav buttons
+        page_nav_div = self.page_soups[0].find("div", attrs={"class": "Pagination__Buttons-uamu6s-5"})      # Get the div with the page-nav buttons
         number_pages = 1
         if page_nav_div:
             number_pages = int(page_nav_div.find_all('a')[-1].string)       # Gets the last page number
-        
+
         for page_number in range(1, number_pages):
             page_link = self.url + "&page=" + str(page_number)
             self.page_soups.append(self._fetch(page_link))
-            debug("sidnummer: " + page_number)
+        debug("{0} pages were fetched".format(len(self.page_soups)))
 
     def _get_categories(self):
         '''Gets the categories from blocket'''
@@ -101,10 +100,10 @@ class MonitoredCategory:
 
     def _get_table_name(self):
         '''Gets the MySQL table from the categories'''
-        self.table = self.categories[1]
+        self.db_table = self.categories[1]
         for i in range(2, len(self.categories)):
-            self.table += "_" + self.categories[i]
-        debug(self.table)
+            self.db_table += "_" + self.categories[i]
+        debug(self.db_table)
 
     def _load_active_ads(self):
         '''Tries to locate the category's table if it exists to load the ads'''
@@ -112,16 +111,16 @@ class MonitoredCategory:
 
         missing = True
         for table in db_cursor:
-            if table[0] == self.table:
+            if table[0] == self.db_table:
                 missing = False
                 break
 
         if missing:
-            debug("Created table " + self.table)
-            db_cursor.execute("CREATE TABLE {0} (url VARCHAR(255), ad_id INTEGER(15), archived BOOLEAN, unique_id INTEGER AUTO_INCREMENT PRIMARY KEY)".format(self.table))
+            debug("Created table " + self.db_table)
+            db_cursor.execute("CREATE TABLE {0} (url VARCHAR(255), ad_id INTEGER(15), archived BOOLEAN, unique_id INTEGER AUTO_INCREMENT PRIMARY KEY)".format(self.db_table))
         else:
             # TRY TO LOAD DATA FROM IT!!!
-            db_cursor.execute("SELECT url, ad_id FROM {0} WHERE archived = false".format(self.table))
+            db_cursor.execute("SELECT url, ad_id FROM {0} WHERE archived = false".format(self.db_table))
             for data in db_cursor:
                 self.active_ad_links.append(data[0])
                 self.ad_ids.append(data[1])
@@ -130,13 +129,16 @@ class MonitoredCategory:
 
     def refresh_ads(self, fetch=True):
         '''Finds new ads and saves them to the DB and updates newly archived ads'''
+        if fetch:
+            self._fetch_all()
+        
         new_ad_links = []
         removed_ad_links = self.active_ad_links.copy()       # Deleting all found links until only the removed ones remain
 
 
         for page_soup in self.page_soups:
             for ad in page_soup.findAll("div", attrs={"class": "styled__Wrapper-sc-1kpvi4z-0"}):                    # Loop over every ad container
-                debug("YES " + ad["to"])
+                debug("Currently processing: " + ad["to"])
                 if ad["to"] in self.active_ad_links:       # Attribute 'to' is the relative link to the ad
                     debug("Removing already found ad" + ad["to"])
                     removed_ad_links.remove(ad["to"])
@@ -149,15 +151,15 @@ class MonitoredCategory:
                         warning("UPDATE THE AD IN THE DATABASE")
                         #self.ad_ids[ad_id].update()
                     else:
-                        debug("New ads id: " + str(ad_id))
+                        debug("New ad's id: " + str(ad_id))
                         self.active_ad_links.append(ad["to"])
                         self.ad_ids.append(int(ad_id))
                         warning("INSERT THE AD INTO THE DATABASE")
                         #self.ad_ids[ad_id] = self.ad_class(ad["to"])  # Using different classes for the ads depending on their categories
                         debug("Active ad ids: {0}".format(len(self.ad_ids)))
-                debug(removed_ad_links)
         
-        debug("Reached", True)
+        debug("\n\nAll ads have been fetched\n\n")
+        
         for removed_ad_link in removed_ad_links:        # Remove removed_links from active_links
             debug('Removed ' + removed_ad_link)
             self.active_ad_links.remove(removed_ad_link)
@@ -172,6 +174,9 @@ class MonitoredCategory:
         # 1. Fetch all standard data
         # 2. Fetch other data depending on the ads table/categories (same result)
         # 3. See if the ad has an earlier record. Update or insert?
+
+
+# --- MAIN ---------------
 
 # Connnect to MySQL
 db = mysql.connector.connect(
