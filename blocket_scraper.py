3de86e4a5880e296d785181f1c76b5674eceb5e5
eskilWinstedt <eggewinstedt@gmail.com>
2020-09-11 14:12:23 +0200

    complete table creation query and ad data insertion

--- blocket_scraper.py
+++ blocket_scraper.py
@@ -11,8 +11,10 @@ from urllib.parse import urlparse
 '''
 TO DO:
     Test timestamp method
-    Fix preliminary update method
+    Add update to update method 
     Add a smart table creator depending on the search querys category
+    Write load ads method
+    Fix Null values in db
 '''
 
 
@@ -55,16 +57,17 @@ def get_ad_id(ad_link):
 
 # --- CLASSES -------------
 class Ad:
-    def __init__(self, url, id):
+    def __init__(self, url, id, db_table):
         self.url = url
         self.id = id
+        self.db_table = db_table
         self.timestamp = None
         self.removed_timestamp = None
         self.location = None
         self.title = None
         self.price = None
         self.description = None
-        self.picture_links = {}
+        self.picture_ids = []
 
         debug("An Ad instance is initialised")
         #self.update()
@@ -91,14 +94,14 @@ class Ad:
         '''Sets the description of the ad'''
         description = self.soup.find('div', attrs={'class': 'BodyCard__DescriptionPart-sc-15r463q-2'})
         if description:
-            self.description = description.string  
+            self.description = str(description.string)
 
     def _get_location(self):
         '''Sets the location of an ad if it's identified'''
         location = self.soup.find('a', attrs={'class': 'LocationInfo__StyledMapLink-sc-1op511s-3'})
         if location:
             location = self._soup_replace(location,'<!-- --> (hitta.se)')    # .string doesn't work with HTML-comments
-            self.location = location.string 
+            self.location = str(location.string)
 
     def _get_pictures(self):
         '''Gets all pictures from one ad and saves them in pictures/ under the name AD-ID_PICTURE-ID.jpg.
@@ -106,7 +109,6 @@ class Ad:
 
         pictureboxes = self.soup.findAll('div', attrs={'class': 'LoadingAnimationStyles__PlaceholderWrapper-c75se8-0 jkleoR'}) # There are 4
         pictureboxes = pictureboxes[2]
-        debug(pictureboxes)
         pictureboxes = pictureboxes.findChild()
         pictureboxes = pictureboxes.findChild()
         pictureboxes = pictureboxes.findChild()
@@ -129,8 +131,8 @@ class Ad:
             link = link[:end_index]
 
             picture_id = get_ad_id(link)
+            self.picture_ids.append(picture_id)
             filepath = folder + str(self.id) + '_' + str(picture_id) + '.jpg'
-            self.picture_links[link] = filepath
             if os.path.isfile(filepath): continue   # The picture is already downloaded
             while True:
                 try:
@@ -145,19 +147,20 @@ class Ad:
                     print("An error occured during ad fetching and filesaving. Retrying in 30 seconds")
                     print("Exception: " + str(e))
                     time.sleep(30)
-        debug(self.picture_links)
+        self.picture_ids = str(self.picture_ids).replace(" ", "")
+        debug(self.picture_ids)
 
     def _get_price(self):
         '''Sets the price of and ad if it's specified'''
         price = self.soup.find('div', attrs={'class': 'Price__StyledPrice-crp2x0-0'})
         if price:
-            self.price = price.string
+            self.price = int("".join(c for c in str(price.string) if c in "1234567890"))
 
     def _get_title(self):
         '''Sets the title of the ad if it's identified'''
         title = self.soup.find('h1', attrs={'class': 'Hero__StyledSubject-sc-1mjgwl-4'})
         if title:
-            self.title = title.string
+            self.title = str(title.string)
 
     def printable_line(self, data, row_length):
         data = data.replace('\n', '')
@@ -258,10 +261,10 @@ class Ad:
         return BeautifulSoup(str(soup).replace(remove, replace), 'html.parser')       
 
     def update(self):
-        '''Updates the ads data'''
+        '''Updates the ads db entry'''
         self._fetch()
-        #self._set_timestamp()
-        #self._get_location()
+        self._set_timestamp()
+        self._get_location()
         self._get_title()
         self._get_price()
         self._get_description()
@@ -269,9 +272,31 @@ class Ad:
         self.__repr__()
         debug(self.url)
         debug(self.id)
-        db_cursor.execute("UPDATE `blocket_scraper`.`fordon_bilar_volkswagen_bubbla` SET `url` = \"{0}\" WHERE `ad_id` = {1};".format(self.url, self.id))
+
+        sql = "INSERT INTO {0} (url, ad_id, titel, timestamp, location, price, description, pictures) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)".format(self.db_table)
+        values = (self.url, self.id, self.title, int(self.timestamp), self.location, self.price, self.description, self.picture_ids)
+
+        for i, e in enumerate(values):
+            debug("{0}: {1}, {2}".format(i, e, type(e)))
+        debug(sql % values)        
+
+        db_cursor.execute(sql, values)
+        db.commit()
+
+
+        '''db_cursor.execute("SELECT COUNT(1) FROM {0} WHERE ad_id = {1}".format(self.db_table, self.id))
+        exists = False
+        
+        for msg in db_cursor:
+            if debug(msg[0]) == 1:
+                exists = True
+                break'''
+        
+        
+
+        '''db_cursor.execute("UPDATE `blocket_scraper`.`fordon_bilar_volkswagen_bubbla` SET `url` = \"{0}\" WHERE `ad_id` = {1};".format(self.url, self.id))
         for data in db_cursor:
-            debug(data)
+            debug(data)'''
 
 
 class MonitoredCategory:
@@ -297,7 +322,7 @@ class MonitoredCategory:
             pass
         else:
             print('absolute ad link: ' + absolute_ad_link)
-            return Ad(absolute_ad_link, id)     # Returns the instance reference
+            return Ad(absolute_ad_link, id, self.db_table)     # Returns the instance reference
 
     def _fetch(self, url):
         '''Takes an url to a page as the argument, fetches the page and returns it as a soup'''
@@ -342,7 +367,7 @@ class MonitoredCategory:
 
 
         for page_soup in self.page_soups:
-            for ad in page_soup.findAll("div", attrs={"class": "styled__Wrapper-sc-1kpvi4z-0"}):                    # Loop over every ad container
+            for ad in page_soup.findAll("div", attrs={"class": "styled__Wrapper-sc-1kpvi4z-0"}):      # Loop over every ad container
                 ad_link = ad["to"]
                 debug("Currently processing: " + ad_link)
                 if ad_link in self.active_ad_links:       # Attribute 'to' is the relative link to the ad
@@ -352,18 +377,17 @@ class MonitoredCategory:
                     self.active_ad_links.append(ad_link)
                     new_ad_links.append(ad_link)
                     ad_id = get_ad_id(ad_link)
-                    if ad_id in self.ad_ids:   # The ads titel (and maybe more) has been changed
+                    if ad_id in self.ad_ids:   # The ads titel (and likely more) has been changed
                         debug("THE AD NAME HAS BEEN CHANGED")
                         warning("UPDATE THE AD IN THE DATABASE")
                         ad_instance = self._ad_class(ad_link, ad_id)
                         ad_instance.update()
-                        #self.ad_ids[ad_id].update()
                     else:
                         debug("New ad's id: " + str(ad_id))
                         self.active_ad_links.append(ad_link)
                         self.ad_ids.append(int(ad_id))
-                        warning("INSERT THE AD INTO THE DATABASE")
-                        #self.ad_ids[ad_id] = self._ad_class(ad_link)  # Using different classes for the ads depending on their categories
+                        ad_instance = self._ad_class(ad_link, ad_id)
+                        ad_instance.update()
                         debug("Active ad ids: {0}".format(len(self.ad_ids)))
         
         debug("\n\nAll ads have been fetched\n\n")
@@ -395,15 +419,19 @@ class MonitoredCategory:
 
         if missing:
             debug("Created table " + self.db_table)
-            db_cursor.execute("CREATE TABLE {0} (url VARCHAR(255), ad_id INTEGER(15) AUTO_INCREMENT PRIMARY KEY, archived BOOLEAN)".format(self.db_table))
+            # All columns are based on blocket's limits plus extra bytes for >1 byte chars. Room for 30 picture ids
+            columns = "url VARCHAR(255), ad_id INT PRIMARY KEY, titel VARCHAR(70), timestamp VARCHAR(30), archived VARCHAR(30), location VARCHAR(80), price INT, description VARCHAR(4000), pictures VARCHAR(331)"
+            db_cursor.execute("CREATE TABLE {0} ({1})".format(self.db_table, columns))
         else:
             # TRY TO LOAD DATA FROM IT!!!
-            db_cursor.execute("SELECT url, ad_id FROM {0} WHERE archived = false".format(self.db_table))
+            db_cursor.execute("SELECT url, ad_id FROM {0} WHERE archived IS NULL".format(self.db_table))
             for data in db_cursor:
                 self.active_ad_links.append(data[0])
                 self.ad_ids.append(data[1])
             debug(self.active_ad_links)
             debug(self.ad_ids)
+
+        
 
     '''def _save_ad(self, url):
         ''''''Saves an ad from its URL in the table located by the ads categories''''''
@@ -431,16 +459,12 @@ db = mysql.connector.connect(
 
 db_cursor = db.cursor(buffered=True)
 
-db_cursor.execute("SHOW DATABASES")
-exists = False
-
-for database in db_cursor:
+'''for database in db_cursor:
     if database[0] == DB_NAME:
         exists = True
-        break
+        break'''
 
-if not exists:
-    db_cursor.execute("CREATE DATABASE {0}".format(DB_NAME))
+db_cursor.execute("CREATE DATABASE IF NOT EXISTS {0}".format(DB_NAME))
 
 db_cursor.execute("USE {0}".format(DB_NAME))
 
